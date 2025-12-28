934f7691589b4e1722f5ace4bbee6e7a768af422
LampByLit <k.ryan.hartley@gmail.com>
2025-12-27 18:54:59 -0800

    Add favicon from lampbylit.com
    - Download favicon.png from https://lampbylit.com/magazine/wp-content/uploads/2020/12/favicon.png
    - Place in public/favicon.png
    - Update layout.tsx metadata to reference favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Secret Santa",
   description: "Automated Secret Santa using Tom7's cryptographic method",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
